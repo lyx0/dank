83816acee4f754473fa62270ab2e2f4e366f4a92
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-11-12 01:02:55 +0100

    try out gorilla mux

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("xd")
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/products", ProductsHandler)
+	r.HandleFunc("/articles", ArticlesHandler)
+	http.ListenAndServe(":8080", r)
+}
+
+func HomeHandler(rw http.ResponseWriter, r *http.Request) {
+	rw.Write([]byte("Home"))
+}
+
+func ProductsHandler(rw http.ResponseWriter, r *http.Request) {
+	rw.Write([]byte("Products"))
+}
+
+func ArticlesHandler(rw http.ResponseWriter, r *http.Request) {
+	rw.Write([]byte("Articles"))
 }
