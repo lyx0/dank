0f8f04d450266a46eae6e3d33f7d4bf77a014eee
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-12-21 18:42:24 +0100

    restart

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,5 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/gorilla/mux"
-	"github.com/lyx0/dank/handlers"
-	"github.com/sirupsen/logrus"
-)
-
-var (
-	log = logrus.New()
-)
-
 func main() {
-
-	bh := handlers.CheckMessage(log)
-
-	sm := mux.NewRouter()
-	postRouter := sm.Methods(http.MethodPost).Subrouter()
-	postRouter.HandleFunc("/message", bh)
 
 }
